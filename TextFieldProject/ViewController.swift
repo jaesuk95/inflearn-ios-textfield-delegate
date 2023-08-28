0e94b9e6ea9bd23954e81c81cdf7b805f8a73ce5
Jaesuk <jaesuk95@outlook.com>
2023-08-28 22:21:10 +0900

    TextField 사용법

--- TextFieldProject/ViewController.swift
+++ TextFieldProject/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // textField
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setup()
     }
 
+    func setup() {
+        view.backgroundColor = UIColor.gray
+        
+        // keyboard setting
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "이메일 입력"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .go   // <- changes the 'done' button
+    }
 
+    // done button
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
