e253838ee37e9207ba18f150acf15fbc064fcea8
Jaesuk <jaesuk95@outlook.com>
2023-08-28 22:48:19 +0900

    textField delegate 개념 이해해기

--- TextFieldProject/ViewController.swift
+++ TextFieldProject/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     // textField
     @IBOutlet weak var textField: UITextField!
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+        
         setup()
     }
 
@@ -30,6 +33,56 @@ class ViewController: UIViewController {
         textField.returnKeyType = .go   // <- changes the 'done' button
     }
 
+    // textField 의 입력을 시작할 때 호출 (시작할지 말지의 여부 허락하는 것)
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    // 시점
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+        print("유저가 텍스트필드의 입력을 시작했다.")
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    // textField 글자 내용이 (한글자 한글자) 입력되거나 지워질 때 호출리 되고 (허락)
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(#function)
+        print(string)
+        return true
+    }
+    
+    // textField 엔터키가 눌러지면 다음 동작을 허락할 것인지
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        
+        if textField.text == "" {
+            textField.placeholder = "Type Something!"
+            return false
+        } else {
+            return true
+        }
+        
+    }
+    
+    // textField 의 입력이 끝날 때 호출 (끝날지 말지를 허락)
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    // textField 입력이 실제 끝날을 때 호출 (시점)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        print("User has finished texting")
+        return
+    }
+    
     // done button
     @IBAction func doneButtonTapped(_ sender: UIButton) {
         
